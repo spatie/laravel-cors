a48cdf59362e82c5b4213073c7491f3f09dd4c23
Benjamin Crozat <benjamin@croz.at>
2018-02-09 15:14:34 +0100

    Added support for Lumen

--- src/CorsServiceProvider.php
+++ src/CorsServiceProvider.php
@@ -11,9 +11,9 @@ class CorsServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        if ($this->app->runningInConsole()) {
+        if ($this->isNotLumen() && $this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../config/cors.php' => config_path('cors.php'),
+                __DIR__ . '/../config/cors.php' => config_path('cors.php'),
             ], 'config');
         }
 
@@ -28,6 +28,13 @@ class CorsServiceProvider extends ServiceProvider
 
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/cors.php', 'cors');
+        if ($this->isNotLumen()) {
+            $this->mergeConfigFrom(__DIR__ . '/../config/cors.php', 'cors');
+        }
+    }
+
+    protected function isNotLumen()
+    {
+        return ! preg_match('/lumen/i', app()->version());
     }
 }
